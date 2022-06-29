ecbfa4b5809b368adde2bc8345a9d2fc182cd245
renanpl <renanp.info@gmail.com>
2022-06-29 16:51:57 -0300

    Criação dos elementos do rodapé

---
+++ partes/rodape.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rodape</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 footer-center-item">
+                <ul class="list-footer">
+                    <li class="title-list">Teste</li>
+                    <li class="item-footer">a</li>
+                    <li class="item-footer">b</li>
+                    <li class="item-footer">c</li>
+                    <li class="item-footer">d</li>
+                </ul>
+            </div>
+            <div class="col-4 footer-center-item">
+                <ul class="list-footer">
+                    <li class="title-list">Teste</li>
+                    <li class="item-footer">a</li>
+                    <li class="item-footer">b</li>
+                    <li class="item-footer">c</li>
+                    <li class="item-footer">d</li>
+                </ul>
+            </div>
+            <div class="col-4 footer-center-item">
+                <ul class="list-footer">
+                    <li class="title-list">Teste</li>
+                    <li class="item-footer">a</li>
+                    <li class="item-footer">b</li>
+                    <li class="item-footer">c</li>
+                    <li class="item-footer">d</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+</html>
